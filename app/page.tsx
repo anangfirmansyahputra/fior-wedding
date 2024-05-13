18f42398cc0a610804e43cada8f93950f51f78e5
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-05-13 15:45:35 +0800

    add keyword

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import About from "@/components/about";
 import Hero from "@/components/hero";
-import HireUs from "@/components/hire-us";
-import Package from "@/components/package";
 import SectionWelcome from "@/components/section-welcome";
-import Services from "@/components/services";
 import Template from "@/components/template";
 import Testimonial from "@/components/testimonial";
 
