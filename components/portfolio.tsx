b2c5e703e331cb807143c17286a70d79aa93ad8f
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-05-14 14:35:18 +0800

    fix: gap on article

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -56,7 +56,7 @@ export default async function Portfolio({ data }: PropsPortfolio) {
           >
             ARTICLE
           </h2>
-          <div className="grid gap-x-5 md:grid-cols-2 lg:grid-cols-3">
+          <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
             {data.map((item) => (
               <Card
                 key={item.id}
