d852ecddc868c441a098ca4e7e81262565991a92
anangfirmansyahputra <anangfirmansyahp5@gmail.com>
2024-07-18 17:13:08 +0800

    fix: create icon

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,6 @@ export const metadata: Metadata = {
     "Fior Wedding Organizer adalah perusahaan perencana pernikahan profesional yang menyediakan layanan lengkap untuk membuat hari istimewa Anda menjadi momen yang tak terlupakan.",
   keywords:
     "Fior Wedding Organizer,perencana pernikahan,organizer pernikahan,wedding planner,paket pernikahan,layanan pernikahan",
-  openGraph: {
-    title: "Anang - Fior Wedding Organizer",
-    description:
-      "Fior Wedding Organizer adalah perusahaan perencana pernikahan profesional yang menyediakan layanan lengkap untuk membuat hari istimewa Anda menjadi momen yang tak terlupakan.",
-    images: [
-      {
-        url: "https://fiororganizer.com/_next/image?url=%2Flogo.png&w=256&q=75",
-        width: 800,
-        height: 600,
-        alt: "Fior Wedding Organizer",
-      },
-    ],
-  },
-  icons: "https://fiororganizer.com/_next/image?url=%2Flogo.png&w=256&q=75",
 };
 
 export default function Home() {
